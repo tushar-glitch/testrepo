54fa9f7d02960fe4b4fd93bc0dc040353634e971
tushar-glitch <tusharc20001@gmail.com>
2023-02-03 12:35:35 +0530

    test commit

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const mongoose = require('mongoose')
 app.use(cors())
 app.use(express.json())
 
-
+const connection_url = 'mongodb://jumbo:tcxCKjlxOsj1vdNe@ac-vi9pq53-shard-00-00.ktnuczj.mongodb.net:27017,ac-vi9pq53-shard-00-01.ktnuczj.mongodb.net:27017,ac-vi9pq53-shard-00-02.ktnuczj.mongodb.net:27017/?ssl=true&replicaSet=atlas-8zajnt-shard-0&authSource=admin&retryWrites=true&w=majority'
 // app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost/test')
-const db = mongoose.connection
-db.on('open', () => {
-    console.log('Database connected successfully');
+mongoose
+.connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log("MongoDB connected");
 })
 
 app.get('/test_route', function (req, res) {
